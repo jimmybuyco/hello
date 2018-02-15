d1d2c8fa8c24b6f320c75946cf45a057febcae41
jimmy <jimmy.buyco@hilsoftinc.com>
2018-02-15 16:28:20 +0800

    fix

--- www/js/index.js
+++ www/js/index.js
@@ -16,34 +16,56 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+var interval = 1000;
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+    onDeviceReady: function () {
+
+        app.receivedEvent('deviceready')
+        // app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    receivedEvent: function (id) {
+        update();
     }
-};
+
+
+}
+
+function update() {
+    $("#name").val(x);
+    x++;
+    water++;
+    $("#water").text(water);
+    setTimeout(update, interval);
+}
+
+function resetButton() {
+    x=0;
+    $("#name").val(x);
+}
+
+function addButton() {
+    x+=10;
+    $("#name").val(x);
+}
+
+
+
+
+
+
